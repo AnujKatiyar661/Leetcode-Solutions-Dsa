1710f012129844914ec703b98b546575707eaa2e
anuj katiyar <106858176+AnujKatiyar661@users.noreply.github.com>
2022-11-23 02:06:42 +0530

    Time: 13 ms (6.74%), Space: 42.6 MB (12.49%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String toLowerCase(String s) {
+      String  result = "";
+        for( char c :s.toCharArray()){
+            if(Character.isUpperCase(c)){
+                result =result+ (char)(c+32);
+            }
+            else{
+                result =result +c;
+
+            }
+            
+        }
+        return result;
+    }
+}
