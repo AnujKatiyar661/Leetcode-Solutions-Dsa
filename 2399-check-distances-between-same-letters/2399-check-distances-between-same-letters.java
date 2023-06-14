549e6053a590276255402dded43d814b67324e90
anuj katiyar <106858176+AnujKatiyar661@users.noreply.github.com>
2023-06-15 00:36:12 +0530

    Time: 1 ms (100.00%), Space: 41.6 MB (97.29%) - LeetHub

---
+++ 2399-check-distances-between-same-letters/2399-check-distances-between-same-letters.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean checkDistances(String s, int[] distance) {
+        int[] range = new int[distance.length];
+        Arrays.fill(range, -1);
+
+        for(int i = 0; i < s.length(); ++i) {
+            int index = s.charAt(i) - 'a';
+
+            if(range[index] != -1 && Math.abs(range[index] - i) != distance[index] + 1)
+                return false;
+
+            range[index] = i;
+        }
+
+        return true;
+    }
+}
