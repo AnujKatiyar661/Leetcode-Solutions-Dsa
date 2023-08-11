c090291f45877fdfb2f759517987edc6cfd1b415
anuj katiyar <106858176+AnujKatiyar661@users.noreply.github.com>
2023-08-11 11:33:27 +0530

    Added solution - LeetHub

---
+++ Move all zeroes to end of array - GFG/move-all-zeroes-to-end-of-array.java
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            int[] arr = new int[n];
+            String[] inputLine = br.readLine().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            new Solution().pushZerosToEnd(arr, n);
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    void pushZerosToEnd(int[] arr, int n) {
+          //filling zeros to rest of the dummy array
+        int [] dummy= new int[n];
+        int idx=0;
+        
+        // filling all non zeros element in dummy array by copying from original array.
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]!=0)
+            {
+                dummy[idx]=arr[i];
+                idx++;
+            }
+        }
+        
+        //filling zeros to rest of the dummy array
+        for( int j=idx;j<n;j++)
+        {
+            dummy[j]=0;
+        }
+       
+       // copying dummy array elements to original array.
+        for( int i=0;i<n;i++)
+        {
+            arr[i]=dummy[i];
+        }
+    }
+}
+
+
+
+    
